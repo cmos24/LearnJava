abc13c0054856c004212c7f76918f6f1177e4c13
cmos24 <707889468@qq.com>
2017-07-14 16:15:03 +0800

    还原一下项目文件
    之前将这个项目转换成了maven项目，还原一下

--- SomeDemo1/src/pers/oyxx/someDemo/InterfaceandInherit/AD.java
+++
@@ -1,6 +0,0 @@
-package pers.oyxx.someDemo.InterfaceandInherit;
-
-public interface AD {
-	//ÎïÀí¹¥»÷
-	public void physicAttack();
-}
--- SomeDemo1/src/pers/oyxx/someDemo/InterfaceandInherit/AP.java
+++
@@ -1,6 +0,0 @@
-package pers.oyxx.someDemo.InterfaceandInherit;
-
-public interface AP {
-	//Ä§·¨¹¥»÷
-	public void magicAttack();
-}

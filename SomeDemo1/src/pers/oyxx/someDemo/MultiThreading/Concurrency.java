3e75223f5dfd761060b035379f05faabbcd3a603
cmos24 <707889468@qq.com>
2017-08-24 20:41:42 +0800

    线程2

---
+++ SomeDemo1/src/pers/oyxx/someDemo/MultiThreading/Concurrency.java
@@ -0,0 +1,9 @@
+package pers.oyxx.someDemo.MultiThreading;
+
+/*多线程的同步问题指的是多个线程同时修改一个数据的时候，可能导致的问题 
+
+多线程的问题，又叫Concurrency 问题*/
+//同步
+public class Concurrency {
+
+}
--- SomeDemo1/src/pers/oyxx/someDemo/MultiThreading/PasswordThread.java
+++ SomeDemo1/src/pers/oyxx/someDemo/MultiThreading/PasswordThread.java
@@ -2,7 +2,7 @@ package pers.oyxx.someDemo.MultiThreading;
 
 import java.util.List;
 
-//穷举密码的线程
+//穷举密码的线程——其实这里用的是递归法
 public class PasswordThread extends Thread{
 	private boolean found = false;
     
@@ -30,7 +30,7 @@ public class PasswordThread extends Thread{
         for (short i = '0'; i <= 'z'; i++) {
             char c = (char) i;
             if (!Character.isLetterOrDigit(c))
-                continue;
+                continue;//如果不是字母或者数字就开始下一次循环
             guessPassword[index] = c;
             if (index != guessPassword.length - 1) {
                 generatePassword(guessPassword, index + 1, password);
